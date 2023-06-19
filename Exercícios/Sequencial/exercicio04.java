95d3307302d0cfd79a15d81dce3c37abd51731e9
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-19 00:36:00 -0300

    Add files via upload

---
+++ Exercícios/Sequencial/exercicio04.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio04 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        int numero, horas;
+        double valor, salario;
+
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite seu número, suas horas trabalhadas e o valor que recebe por hora: ");
+            numero = sc.nextInt();
+            horas = sc.nextInt();
+            valor = sc.nextDouble();
+
+        sc.close();
+
+        salario = horas * valor;
+
+        System.out.println("NUMBER = " + numero);
+        System.out.println("SALARY = U$ " + salario);
+
+    }
+
+}
