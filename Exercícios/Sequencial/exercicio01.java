2bd4a7db9ecad4f60f2cf93c0870a2157a425caa
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-19 00:35:13 -0300

    Add files via upload

---
+++ Exercícios/Sequencial/exercicio01.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio01 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        int x, y, soma;
+
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite o primeiro valor: ");
+            x = sc.nextInt();
+            System.out.println("Digite o segundo valor: ");
+            y = sc.nextInt();
+
+        sc.close();
+
+        soma = x + y;
+        System.out.println("Soma = " + soma);
+
+    }
+
+}
