72a58389793fc94874afcea88a59188d658c3a00
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-19 20:29:16 -0300

    Add files via upload

---
+++ Exercícios/Condicional/exercicio06.java
@@ -0,0 +1,42 @@
+package Condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio06 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        double x;
+
+        System.out.print("Digite um número: ");
+
+        Scanner sc = new Scanner(System.in);
+
+            x = sc.nextDouble();
+
+        if (x >= 0 && x <= 100) {
+            if (x <= 25) {
+                System.out.println("Intervalo [0, 25]");
+            }
+            else if (x <= 50 ) {
+                System.out.println("Intervalo [25, 50]");
+            }
+            else if (x <= 75) {
+                System.out.println("Intervalo [50, 75]");
+            }
+            else {
+                System.out.println("Intervalo [75, 100]");
+            }
+        }
+        else {
+            System.out.println("Fora do Intervalo");
+        }
+
+        sc.close();
+
+    }
+
+}
