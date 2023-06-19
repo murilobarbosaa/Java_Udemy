1d35aff3398b65a208843bf72a0d4ec77ad52e35
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-19 00:36:13 -0300

    Add files via upload

---
+++ Exercícios/Sequencial/exercicio05.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio05 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        int c1, n1, c2, n2;
+        double v1, v2, total;
+
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite o código, o número e o valor das peças: ");
+            c1 = sc.nextInt();
+            n1 = sc.nextInt();
+            v1 = sc.nextDouble();
+            c2 = sc.nextInt();
+            n2 = sc.nextInt();
+            v2 = sc.nextDouble();
+
+        sc.close();
+
+        total = (n1 * v1) + (n2 * v2);
+
+        System.out.printf("Valor a pagar: R$ %.2f", total);
+
+    }
+
+}
