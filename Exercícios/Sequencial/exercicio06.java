f0e3ba7c7f0cfb4fbe3c6934f953409fb76b223a
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-19 00:36:30 -0300

    Add files via upload

---
+++ Exercícios/Sequencial/exercicio06.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio06 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        double a, b, c, triangulo, circulo, trapezio, quadrado, retangulo;
+
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("Digite os valores : ");
+            a = sc.nextDouble();
+            b = sc.nextDouble();
+            c = sc.nextDouble();
+
+        sc.close();
+
+        triangulo = (a * c) / 2;
+        circulo = 3.14159 * Math.pow(c, 2.0);
+        trapezio = (a + b) * c / 2;
+        quadrado = b * b;
+        retangulo = a * b;
+
+        System.out.printf("TRIÂNGULO = %.2f %n", triangulo);
+        System.out.printf("CÍRCULO = %.2f %n", circulo);
+        System.out.printf("TRAPÉZIO = %.2f %n", trapezio);
+        System.out.printf("QUADRADO = %.2f %n", quadrado);
+        System.out.printf("RETÂNGULO = %.2f %n", retangulo);
+
+    }
+
+}
