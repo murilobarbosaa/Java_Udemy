3c53d7e0ea1b3a0d460393f1c544a7c104d7d667
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-19 21:10:03 -0300

    Update exercicio03.java

--- Exercícios/Condicional/exercicio03.java
+++ Exercícios/Condicional/exercicio03.java
@@ -9,14 +9,12 @@ public class exercicio03 {
 
         Locale.setDefault(Locale.US);
 
-        int x, y;
-
         System.out.print("Digite 2 números: ");
 
         Scanner sc = new Scanner(System.in);
 
-            x = sc.nextInt();
-            y = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
 
         if (x % y == 0 || y % x == 0)  {
             System.out.println("São múltiplos");
