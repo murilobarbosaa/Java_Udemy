cf25d5156084087ed0e4f347dabfa4ee2da4d797
Murilo Barbosa Cardoso <murilobarbosa2811@gmail.com>
2023-06-23 16:11:02 -0300

    Update exercicio05.java

--- Exercícios/Sequencial/exercicio05.java
+++ Exercícios/Sequencial/exercicio05.java
@@ -7,18 +7,17 @@ public class exercicio05 {
 
         Locale.setDefault(Locale.US);
 
-        int c1, n1, c2, n2;
-        double v1, v2, total;
+        double total;
 
         Scanner sc = new Scanner(System.in);
 
             System.out.println("Digite o código, o número e o valor das peças: ");
-            c1 = sc.nextInt();
-            n1 = sc.nextInt();
-            v1 = sc.nextDouble();
-            c2 = sc.nextInt();
-            n2 = sc.nextInt();
-            v2 = sc.nextDouble();
+            int c1 = sc.nextInt();
+            int n1 = sc.nextInt();
+            double v1 = sc.nextDouble();
+            int c2 = sc.nextInt();
+            int n2 = sc.nextInt();
+            double v2 = sc.nextDouble();
 
         total = (n1 * v1) + (n2 * v2);
 
